d329e66fa25159c2bdb98cb615d252d3d43720c1
wisdom nwakaku <nwakakucc@gmail.com>
2021-11-03 14:47:11 +0100

    kjnfj

--- src/App.js
+++ src/App.js
@@ -10,15 +10,21 @@ import Shop from './components/Shop'
 
 const App = () => {
   return (
-    <Router>
     <div>
-      <Switch>
-        <Route exact path="/ixuh">
-          <Body/>
-        </Route>
-      </Switch>
+      hello world
     </div>
-  </Router>
+  //   <Router>
+  //   <div>
+  //     <Switch>
+  //       <Route exact path="/">
+  //         <Body/>
+  //       </Route>
+  //       <Route path="/shop">
+  //         <Shop/>
+  //       </Route>
+  //     </Switch>
+  //   </div>
+  // </Router>
   )
 }
 
